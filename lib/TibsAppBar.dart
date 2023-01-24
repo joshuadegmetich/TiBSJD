f13efcc43ec3e7e95a2cc3601d6b4bebd9dd1cfd
joshuadegmetich <jdegmetich@att.net>
2023-01-23 20:18:08 -0800

    SV-116 make top bar a preferred size widget

--- lib/TibsAppBar.dart
+++ lib/TibsAppBar.dart
@@ -4,8 +4,11 @@ void main() {
   runApp(const MaterialApp(home: TibsAppBar()));
 }
 
-class TibsAppBar extends StatefulWidget {
+class TibsAppBar extends StatefulWidget with PreferredSizeWidget {
   const TibsAppBar({super.key});
+
+  @override
+  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
 
   @override
   State<TibsAppBar> createState() => _TibsAppBarState();
@@ -16,6 +19,11 @@ class _TibsAppBarState extends State<TibsAppBar> {
   Widget build(BuildContext context) {
     return AppBar(
       backgroundColor: const Color(0xFF88B434),
+      leading: IconButton(
+        icon: const Icon(Icons.menu),
+        color: Colors.black,
+        onPressed: () {},
+      ),
       title: Image.asset('assets/tibs-3.png', fit: BoxFit.contain, height: 50),
       actions: <Widget>[
         IconButton(
